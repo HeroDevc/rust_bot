c75bd98036eb37e2b65b1dfd9f1074caf16090a9
HeroDevc <122998737+HeroDevc@users.noreply.github.com>
2025-08-11 14:17:18 +0300

    Add files via upload

--- src/commands/messages.rs
+++ src/commands/messages.rs
@@ -2,12 +2,12 @@ use sqlx::{Pool, Postgres};
 
 use crate::database::db_get_messages;
 
-pub async fn get_messages(player: &String, count: i32, server: String, conn: &Pool<Postgres>) -> String {
+pub async fn get_messages(player: &String, server: String, conn: &Pool<Postgres>) -> String {
     let res = db_get_messages(player.to_string(), server, conn).await;
 
     match res {
         Ok(v) => {
-            let formatted_text = format!("{}: {}", v.player_name, v.count + count);
+            let formatted_text = format!("{}: {}", v.player_name, v.count);
 
             formatted_text
         },
